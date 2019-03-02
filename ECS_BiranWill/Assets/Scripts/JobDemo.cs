d2957c14ef05b80017702ab76cd71943cf468707
risulkarimx1 <risulkarimx1@gmail.com>
2019-03-02 21:10:18 +0100

    [Smuggle out values using native array]

---
+++ ECS_BiranWill/Assets/Scripts/JobDemo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+public class JobDemo : MonoBehaviour
+{
+   struct TestJob : IJob
+   {
+      public int x;
+      public int y;
+
+      public NativeArray<int> array;
+      
+      public void Execute()
+      {
+         array[0] = x + y;
+         Debug.Log($"Hi I am from job: {array[0]}"); // smaggle out via native container
+      }
+   }
+
+   private void Start()
+   {
+      var tj = new TestJob()
+      {
+         x = 3,
+         y = 5,
+         array = new NativeArray<int>(1,Allocator.TempJob)// deletes them from memory if they live longer than 4 frames
+         
+      };
+
+      var jobHandle = tj.Schedule();
+      JobHandle.ScheduleBatchedJobs();
+      
+      jobHandle.Complete();
+
+      Debug.Log($"value of x is {tj.array[0]}");
+      tj.array.Dispose();
+   }
+}

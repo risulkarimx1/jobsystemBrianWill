674761c7b9d6a15ae152343b12724189c16fe285
risulkarimx1 <risulkarimx1@gmail.com>
2019-03-03 20:42:19 +0100

    [E2] using Component data array we can iterate through group of components, get values of the components and change them as well

--- ECS_BiranWill/Assets/Scripts/MySystem.cs
+++ ECS_BiranWill/Assets/Scripts/MySystem.cs
@@ -14,27 +14,59 @@ public struct OtherComponent : IComponentData
     public int Num;
 }
 
+public struct SomeComponent : IComponentData
+{
+    public int Num;
+}
+
 public class MySystem :ComponentSystem
 {
+    private ComponentGroup _group;
     protected override void OnCreateManager()
     {
         Debug.Log($"Created");
+        
+        
+        World world = World.Active;
+        EntityManager em = world.GetOrCreateManager<EntityManager>();
+
+        EntityArchetype archetype1 = EntityManager.CreateArchetype(typeof(MyComponent), typeof(OtherComponent));
+        EntityArchetype archetype2 = EntityManager.CreateArchetype(typeof(MyComponent), typeof(OtherComponent),typeof(SomeComponent));
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            em.CreateEntity(archetype1);
+        }
+
+        for (int i = 0; i < 20; i++)
+        {
+            em.CreateEntity(archetype2);
+        }
+
+        _group = GetComponentGroup(typeof(MyComponent), typeof(OtherComponent));
+
     }
 
     protected override void OnUpdate()
     {
-        World world = World.Active;
-        EntityManager em = world.GetOrCreateManager<EntityManager>();
+        ComponentDataArray<MyComponent> myComponents = _group.GetComponentDataArray<MyComponent>();
+        ComponentDataArray<OtherComponent> otherComponents = _group.GetComponentDataArray<OtherComponent>();
 
-        EntityArchetype archetype = EntityManager.CreateArchetype(typeof(MyComponent), typeof(OtherComponent));
-
-        Entity entity = em.CreateEntity(archetype);
-
-        int index = entity.Index;
-        int version = entity.Version;
-
-        Debug.Log($"index {index} and version {version}");
-
+        EntityArray entities = _group.GetEntityArray();
+        for (int i = 0; i < myComponents.Length; i++)
+        {
+            // we can read component data
+            Entity entity = entities[i];
+            MyComponent myComponent = myComponents[i];
+            OtherComponent otherComponent = otherComponents[i];
+            // we can mutate component data
+            otherComponents[i] = new OtherComponent(){Num =  55};
+            
+            /// we can destroy entity manager while iterating
+            // we can user something call entitiy command buffer 
+            PostUpdateCommands.DestroyEntity(entity);
+        }
     }
 
     protected override void OnDestroyManager()

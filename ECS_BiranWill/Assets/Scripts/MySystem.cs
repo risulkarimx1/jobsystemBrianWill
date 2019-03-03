083f8ee53f8d1534ef76d58b20e23e3b2d8dbcc8
risulkarimx1 <risulkarimx1@gmail.com>
2019-03-03 13:49:48 +0100

    [E2] Basic setup of ECS.. Imported Entity package from Package Manager and then make a couple of Systems

---
+++ ECS_BiranWill/Assets/Scripts/MySystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+
+public class MySystem :ComponentSystem
+{
+    protected override void OnCreateManager()
+    {
+        Debug.Log($"Created");
+    }
+
+    protected override void OnUpdate()
+    {
+        Debug.Log($"hi from update");
+    }
+
+    protected override void OnDestroyManager()
+    {
+        Debug.Log($"Destroyed");
+    }
+}
+
+
+public class MySystem2 :ComponentSystem
+{
+    protected override void OnCreateManager()
+    {
+        Debug.Log($"Created2");
+    }
+
+    protected override void OnUpdate()
+    {
+        Debug.Log($"hi from update2");
+    }
+
+    protected override void OnDestroyManager()
+    {
+        Debug.Log($"Destroyed2");
+    }
+}
